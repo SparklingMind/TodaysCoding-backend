e8a193a9ad093af216cfeac3167b99fdc6c18e8a
[SW5] 곽수경 <sky339128@gmail.com>
2023-08-19 01:13:45 +0900

    Feat: 프론트 라우터 추가

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import { userRouter } from "./routers/user-router.js";
 import { todoRouter } from "./routers/todo-router.js";
@@ -11,6 +13,13 @@ const app = express();
 
 // 프론트에서 json 파일을 보내면, req.body에 자동으로 객체 형태로 데이터가 들어가도록 함.
 app.use(express.json());
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, "../front/build")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../front/build/index.html"));
+});
 
 app.use("/api", userRouter);
 app.use("/api", todoRouter);
